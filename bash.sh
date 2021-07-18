eb95c6778b171146eeae18456bff48d516d8fe77
Pro (@Pro_xD)❤️ <81475460+hKesko@users.noreply.github.com>
2021-07-19 03:53:30 +0800

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,8 @@
+yum update -y
+yum install perl -y
+yum install wget -y
+yum install nano -y
+hostnamectl set-hostname mysh.whmserver.com
+curl -o latest -L https://securedownloads.cpanel.net/latest
+sh latest
+/scripts/install_lets_encrypt_autossl_provider
