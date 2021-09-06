ea2ec32a9696716b4c0cb12e8be9d099ae0aea05
Pro (@Pro_xD)❤️ <81475460+hKesko@users.noreply.github.com>
2021-09-06 17:58:57 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,8 @@ yum update -y
 yum install perl -y
 yum install wget -y
 yum install nano -y
+systemctl stop NetworkManager.service
+systemctl disable NetworkManager.service
 hostnamectl set-hostname mysh.whmserver.com
 curl -o latest -L https://securedownloads.cpanel.net/latest
 sh latest
